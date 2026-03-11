2e89c1cb35df4bd8772f40ef72da8f6d09fd1cb2
Prathmesh  Kulkarni <prathmeshkulkarni@Prathmeshs-MacBook-Air.local>
2026-03-11 04:15:45 -0400

    Product

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -8,7 +8,7 @@ const person: Person = {
   role: "AI Product Management",
   avatar: "/images/avatar.jpg",
   email: "pkulkarni590@gmail.com",
-  location: "Baltimore, MD", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  location: "America/New_York", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English", "Hindi", "Marathi"], // optional: Leave the array empty if you don't want to display languages
 };
 
